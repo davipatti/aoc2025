49fadac12f1473283f7c965ce04f7bb974ff53df
David Pattinson <david.pattinson@wisc.edu>
2025-12-02 09:04:01 -0500

    Day 2.

---
+++ day-02/main.py
@@ -0,0 +1,79 @@
+# %%
+
+
+from typing import Callable
+
+
+def read_input(path):
+    with open(path) as fobj:
+        for line in fobj:
+            for pair in line.strip().split(","):
+                yield map(int, pair.split("-"))
+
+
+# %%
+
+
+def solve(path: str, is_invalid: Callable) -> int:
+    return sum(
+        id
+        for a, b in read_input(path)
+        for id in range(a, b + 1)
+        if is_invalid(id)
+    )
+
+
+# %%
+
+
+def is_invalid_part_1(id: str | int) -> bool:
+    id = str(id)
+
+    id_len = len(id)
+
+    if id_len % 2 == 0:
+        middle = id_len // 2
+        return id[:middle] == id[middle:]
+
+    else:
+        return False
+
+
+assert is_invalid_part_1("11")
+assert is_invalid_part_1("38593859")
+assert not is_invalid_part_1("1698528")
+
+print(solve("input", is_invalid_part_1))
+
+# %%
+
+
+def grouper(iterable, n):
+    iterators = [iter(iterable)] * n
+    return zip(*iterators, strict=True)
+
+
+def is_invalid_part_2(id: str | int) -> bool:
+    id = str(id)
+    for n in range(1, (len(id) // 2) + 1):
+
+        try:
+            # unique length n chunks of id
+            unique_chunks = set(grouper(id, n))
+        
+        except ValueError:
+            # if n isn't a divisor of len(id), a ValueError is thrown
+            continue
+
+        if len(set(unique_chunks)) == 1:
+            return True
+    
+    else:
+        return False
+
+
+assert is_invalid_part_2("446446")
+assert is_invalid_part_2("2121212121")
+assert not is_invalid_part_2("2121212124")
+
+print(solve("input", is_invalid_part_2))
