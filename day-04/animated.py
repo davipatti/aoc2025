768745b0065a7f35459f70af1918bba6469d525f
David Pattinson <david.pattinson@wisc.edu>
2025-12-04 10:29:56 -0500

    cleaning up animated.py a bit

--- day-04/animated.py
+++ day-04/animated.py
@@ -1,5 +1,3 @@
-# %%
-
 from main import read_input, KERNEL
 
 from scipy import signal
@@ -16,31 +14,20 @@ def plot(n):
 def animate_remove_rolls(grid: np.ndarray, fig_num: int) -> np.ndarray:
     num_rolls_orig = grid.sum()
 
-    # Save a figure
     plot(fig_num)
     fig_num += 1
 
-    result = signal.convolve2d(grid, KERNEL, mode="same").astype(float)
+    n_neighbours = signal.convolve2d(grid, KERNEL, mode="same")
 
-    # replace empty slots with nan
-    result[grid == 0] = np.nan
+    grid[n_neighbours < 4] = 0
 
-    # indexes of rolls to remove
-    idx = np.argwhere(result < 4)
-
-    # remove accessible rolls
-    grid[idx[:, 0], idx[:, 1]] = 0
-
-    num_rolls_now = grid.sum()
-
-    # keep trying to remove rolls if rolls were just removed
     return (
         animate_remove_rolls(grid, fig_num)
-        if num_rolls_now != num_rolls_orig
+        if grid.sum() != num_rolls_orig
         else grid
     )
 
 
 if __name__ == "__main__":
-    grid = np.array(list(read_input("input")))
+    grid = read_input("input")
     animate_remove_rolls(grid, fig_num=0)
