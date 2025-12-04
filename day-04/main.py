2d1b45bd8f0eb9acf53a14fdc087279da53ab707
David Pattinson <david.pattinson@wisc.edu>
2025-12-04 09:52:28 -0500

    part1 and part2 using the same 'remove_rolls' func

--- day-04/main.py
+++ day-04/main.py
@@ -4,51 +4,44 @@ from scipy import signal
 
 def read_input(path):
     with open(path) as fobj:
-        for line in fobj:
-            yield [0 if char == "." else 1 for char in line.strip()]
+        return np.array(
+            [
+                [0 if char == "." else 1 for char in line.strip()]
+                for line in fobj
+            ]
+        )
 
 
 KERNEL = np.array([[1, 1, 1], [1, 0, 1], [1, 1, 1]])
 
 
-def part1(input_path):
-    grid = np.array(list(read_input(input_path)))
-    result = signal.convolve2d(grid, KERNEL, mode="same")
-    return (result[grid == 1] < 4).sum()
+def remove_accessible_rolls(grid: np.ndarray, recursive: bool) -> np.ndarray:
+    n_rolls_orig = grid.sum()
 
+    n_neighbours = signal.convolve2d(grid, KERNEL, mode="same").astype(float)
 
-def remove_accessible_rolls(grid: np.ndarray) -> np.ndarray:
-    num_rolls_orig = grid.sum()
-
-    result = signal.convolve2d(grid, KERNEL, mode="same").astype(float)
-
-    # replace empty slots with nan
-    result[grid == 0] = np.nan
-
-    # indexes of rolls to remove
-    idx = np.argwhere(result < 4)
+    n_neighbours[grid == 0] = np.nan  # empty slots -> nan
 
     # remove accessible rolls
+    idx = np.argwhere(n_neighbours < 4)
     grid[idx[:, 0], idx[:, 1]] = 0
-
-    num_rolls_now = grid.sum()
 
     # keep trying to remove rolls if rolls were just removed
     return (
-        remove_accessible_rolls(grid)
-        if num_rolls_now != num_rolls_orig
+        remove_accessible_rolls(grid, recursive)
+        if recursive and grid.sum() != n_rolls_orig
         else grid
     )
 
 
-def part2(input_path):
-    grid = np.array(list(read_input(input_path)))
+def solve(input_path, recursive):
+    grid = read_input(input_path)
 
     # number of rolls removed is the number of initial rolls - number of
     # ending rolls
-    return grid.sum() - remove_accessible_rolls(grid).sum()
+    return grid.sum() - remove_accessible_rolls(grid, recursive).sum()
 
 
 if __name__ == "__main__":
-    print(part1("input"))
-    print(part2("input"))
+    print(solve("input", recursive=False))  # 1516
+    print(solve("input", recursive=True))  # 9122
