2495bec3f983beadfcf4276ede092b98e17b42e6
David Pattinson <david.pattinson@wisc.edu>
2025-12-11 17:05:11 -0500

    moved plotting to lib.py

--- day-11/lib.py
+++ day-11/lib.py
@@ -23,3 +23,59 @@ def plot_graph(g, **kwds):
         linewidths=0.5,
         **kwds,
     )
+
+
+def load_nx_graph(path):
+    with open(path) as fobj:
+        d = {}
+        for line in fobj:
+            parent, *children = line.strip().split()
+            d[parent.strip(":")] = children
+    return nx.DiGraph(d)
+
+
+def load_subgraph(path, start, end):
+    g = load_nx_graph(path)
+
+    # Anything that isn't a descendant of start (keep start)
+    g.remove_nodes_from(set(g.nodes()) - (nx.descendants(g, start) | {start}))
+
+    # Anything that doesn't have end in its descendants
+    g.remove_nodes_from(
+        {node for node in g.nodes() if end not in nx.descendants(g, node)}
+        - {end}
+    )
+
+    return g
+
+
+def plot_subgraphs(path, start_ends):
+    """
+    plot the subgraphs implied by start_end
+    """
+    fig, _ = plt.subplots(
+        nrows=2, ncols=2, figsize=(20, 10), sharex=True, sharey=True
+    )
+    axes = iter(fig.axes)
+    g = load_nx_graph(path)
+    pos = nx.bfs_layout(g, "svr")
+    ax = next(axes)
+    plot_graph(g, pos=pos, ax=ax)
+    ax.set_title("Full graph")
+
+    for start, end in start_ends:
+        assert nx.has_path(g, start, end)
+        subgraph = load_subgraph(path, start, end)
+        ax = ax = next(axes)
+        plot_graph(
+            g,
+            edge_color="lightgrey",
+            node_color="lightgrey",
+            pos=pos,
+            ax=ax,
+        )
+        plot_graph(subgraph, pos=pos, ax=ax)
+        ax.set_title(f"{start} → {end}")
+
+    plt.savefig("part2-subgraphs.png", dpi=300, bbox_inches="tight")
+    plt.close()
--- day-11/main.py
+++ day-11/main.py
@@ -1,10 +1,7 @@
 from functools import cache
 from itertools import pairwise
 
-import matplotlib.pyplot as plt
 import networkx as nx
-
-from lib import plot_graph
 
 
 def part1(path):
@@ -25,21 +22,20 @@ def part2(path):
 
         svr -> fft -> dac -> out
     """
-    g = load_nx_graph("input")
+    g = load_nx_graph(path)
 
     assert nx.has_path(g, "svr", "dac")
     assert nx.has_path(g, "svr", "fft")
 
     assert nx.has_path(g, "fft", "dac")
     assert not nx.has_path(g, "dac", "fft")  # no path from dac -> fft
-
     assert nx.has_path(g, "fft", "out")
     assert nx.has_path(g, "dac", "out")
 
     assert nx.is_directed_acyclic_graph(g)  # this is a DAG
 
     return count_paths_iter(
-        path, start_ends=pairwise(("svr", "fft", "dac", "out")), plot=True
+        g, start_ends=pairwise(("svr", "fft", "dac", "out"))
     )
 
 
@@ -55,47 +51,11 @@ def count_paths(g, start, end):
     return count(start)
 
 
-def count_paths_iter(path, start_ends, plot=False):
-    """
-    Donkey work of part 2
-    
-    Multiply the number of paths in each subgraph.
-    """
-    g = load_nx_graph(path)
-
-    if plot:
-        fig, _ = plt.subplots(
-            nrows=2, ncols=2, figsize=(20, 10), sharex=True, sharey=True
-        )
-        axes = iter(fig.axes)
-        pos = nx.bfs_layout(g, "svr")
-        ax = next(axes)
-        plot_graph(g, pos=pos, ax=ax)
-        ax.set_title("Full graph")
-
+def count_paths_iter(g, start_ends):
+    """Multiply the number of paths in each subgraph."""
     n = 1
     for start, end in start_ends:
-        assert nx.has_path(g, start, end)
-        subgraph = load_subgraph(path, start, end)
-
-        if plot:
-            ax = ax = next(axes)
-            plot_graph(
-                g,
-                edge_color="lightgrey",
-                node_color="lightgrey",
-                pos=pos,
-                ax=ax,
-            )
-            plot_graph(subgraph, pos=pos, ax=ax)
-            ax.set_title(f"{start} → {end}")
-
-        n *= count_paths(subgraph, start, end)
-
-    if plot:
-        plt.savefig("part2-subgraphs.png", dpi=300, bbox_inches="tight")
-        plt.close()
-
+        n *= count_paths(g, start, end)
     return n
 
 
@@ -106,21 +66,6 @@ def load_nx_graph(path):
             parent, *children = line.strip().split()
             d[parent.strip(":")] = children
     return nx.DiGraph(d)
-
-
-def load_subgraph(path, start, end):
-    g = load_nx_graph(path)
-
-    # Anything that isn't a descendant of start (keep start)
-    g.remove_nodes_from(set(g.nodes()) - (nx.descendants(g, start) | {start}))
-
-    # Anything that doesn't have end in its descendants
-    g.remove_nodes_from(
-        {node for node in g.nodes() if end not in nx.descendants(g, node)}
-        - {end}
-    )
-
-    return g
 
 
 if __name__ == "__main__":
