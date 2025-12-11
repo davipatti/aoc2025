2d33d0b61642d927ea537b9652ea9e601c8f16d4
David Pattinson <david.pattinson@wisc.edu>
2025-12-11 08:49:10 -0500

    day 10 part 2

--- day-10/main.py
+++ day-10/main.py
@@ -1,5 +1,6 @@
 from itertools import combinations
 
+from scipy.optimize import milp, LinearConstraint
 import numpy as np
 
 
@@ -40,12 +41,59 @@ def find_comb(buttons, goal):
         n_buttons += 1
 
 
+def count_presses(buttons, target):
+    """
+    scipy milp (mixed integer linear programming) minimizes:
+
+        c @ x
+
+    such that:
+
+        b_l <= A @ x <= b_u     # -> can constrain the result
+        l <= x <= u             # -> can constrain the decision variables
+
+    and elements of x must be integers.
+
+    - `x` is a vector of button presses; the number of times each button is
+      pressed.  `x` is what is being optimized. Also called the 'decision
+      variables'.
+    - `c` can be thought of as a vector of 'weights' for each button. Passing
+      all ones for c sums the total number of button presses (c @ x).
+    - `A` can also constrain the decision variables via A @ x. A encodes the
+      counters that get incremented for each button press.
+    - We set b_l and b_u as the target, so that A @ x must == target (target is
+      the required counts on each counter).
+    """
+    m = len(buttons)
+    n = len(target)
+
+    # A[i, j] is 1 if the ith button turns on the jth light
+    A = np.zeros((n, m))
+    for j, i in enumerate(buttons):
+        A[i, j] = 1
+
+    res = milp(
+        c=np.ones(m),
+        constraints=LinearConstraint(A, lb=target, ub=target),
+        integrality=1,  # forces decision variables to be ints
+    )
+
+    return round(res.x.sum())
+
+
 def part1(path):
     return sum(
         find_comb(buttons, goal) for goal, buttons, _ in load_data(path)
     )
 
 
-if __name__ == "__main__":
+def part2(path):
+    return sum(
+        count_presses(buttons, target)
+        for _, buttons, target in load_data(path)
+    )
 
+
+if __name__ == "__main__":
     print(part1("input"))
+    print(part2("input"))
