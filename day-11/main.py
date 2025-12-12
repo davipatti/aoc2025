a8896b63819b87d286da6ee67e2cdbfd635f7f6a
David Pattinson <davipatti@protonmail.com>
2025-12-11 19:55:09 -0500

    removed networkx dependency from day-11

--- day-11/main.py
+++ day-11/main.py
@@ -1,73 +1,46 @@
 from functools import cache
 from itertools import pairwise
-
-import networkx as nx
-
-
-def part1(path):
-    g = load_nx_graph(path)
-    return len(list(nx.all_simple_paths(g, source="you", target="out")))
+import math
 
 
-def part2(path):
-    """
-    Paths can either go:
-
-        svr -> dac -> fft -> out
-        svr -> fft -> dac -> out
-
-    However, there are no paths from dac -> fft.
-
-    Therefore the only path allowed must go:
-
-        svr -> fft -> dac -> out
-    """
-    g = load_nx_graph(path)
-
-    assert nx.has_path(g, "svr", "dac")
-    assert nx.has_path(g, "svr", "fft")
-
-    assert nx.has_path(g, "fft", "dac")
-    assert not nx.has_path(g, "dac", "fft")  # no path from dac -> fft
-    assert nx.has_path(g, "fft", "out")
-    assert nx.has_path(g, "dac", "out")
-
-    assert nx.is_directed_acyclic_graph(g)  # this is a DAG
-
-    return count_paths_iter(
-        g, start_ends=pairwise(("svr", "fft", "dac", "out"))
-    )
+def load_data(path):
+    with open(path) as fobj:
+        graph = {}
+        for line in fobj:
+            parent, *children = line.strip().split()
+            graph[parent[:-1]] = children
+    return graph
 
 
-def count_paths(g, start, end):
-    """Heart of part 2"""
+def count_paths(graph, start, end):
 
-    @cache  # memoisation only improves runtime from ~4 -> ~0.8 s
-    def count(u):
-        if u == end:
+    @cache
+    def count(node):
+        if node == end:
             return 1
-        return sum(count(v) for v in g.successors(u))
+        elif node not in graph:
+            return 0
+        else:
+            return sum(count(child) for child in graph[node])
 
     return count(start)
 
 
-def count_paths_iter(g, start_ends):
-    """Multiply the number of paths in each subgraph."""
-    n = 1
-    for start, end in start_ends:
-        n *= count_paths(g, start, end)
-    return n
+def count_paths_route(graph, route):
+    return math.prod(count_paths(graph, start, end) for start, end in route)
 
 
-def load_nx_graph(path):
-    with open(path) as fobj:
-        d = {}
-        for line in fobj:
-            parent, *children = line.strip().split()
-            d[parent.strip(":")] = children
-    return nx.DiGraph(d)
+def part1(graph):
+    return count_paths(graph, "you", "out")
+
+
+def part2(graph):
+    route1 = pairwise(("svr", "fft", "dac", "out"))
+    route2 = pairwise(("svr", "dac", "fft", "out"))
+    return count_paths_route(graph, route1) + count_paths_route(graph, route2)
 
 
 if __name__ == "__main__":
-    print(part1("input"))  # 753
-    print(part2("input"))  # 450854305019580
+    graph = load_data("input")
+    print(part1(graph))  # 753
+    print(part2(graph))  # 450854305019580
