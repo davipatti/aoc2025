a59e1c7679c3330ab905a23474f8979e100a1767
David Pattinson <davipatti@protonmail.com>
2025-12-12 21:32:48 -0500

    Day 12

---
+++ day-12/main.py
@@ -0,0 +1,86 @@
+def load_data(path):
+    with open(path) as fobj:
+        shape_def = False
+
+        # Will call the number of grid slots that are occupied in each shape
+        # the shape's 'mass'. E.g. the this shape would have a mass of 7:
+        #
+        #    ..#
+        #    ###
+        #    ###
+        #
+        shape_mass = 0
+        for line in fobj:
+
+            if shape_def:
+                shape_mass += line.count("#")
+                if line.strip() == "":
+                    yield shape_mass
+                    shape_mass = 0
+                    shape_def = False
+
+            elif line[1] == ":":
+                shape_def = True
+
+            elif "x" in line:
+                dims, *counts = line.strip().split()
+                w, h = map(int, dims[:-1].split("x"))
+                counts = tuple(map(int, counts))
+                yield (w, h), counts
+
+
+def enough_grid_cells(grid_w, grid_h, counts, shape_masses):
+    """
+    Can rule out some grid and shape count combinations if there aren't
+    possibly enough grid cells.
+
+    E.g. if one of the lines asked for two of the following shapes:
+
+                ..#
+                ###
+                ###
+
+    in a 4x3 grid, then there is definitely no solution, because together the
+    shapes would require a grid with at least 2x7=14 slots.
+    """
+    area_required = sum(m * c for m, c in zip(shape_masses, counts))
+    grid_area = grid_w * grid_h
+    return area_required <= grid_area
+
+
+def can_trivially_pack(grid_w, grid_h, counts):
+    """
+    Check if a grid can accommodate the number of shapes in counts by simply
+    putting each shape in it's own 3x3 slot.
+    """
+    num_shapes = sum(counts)
+    num_3x3_slots = (grid_w // 3) * (grid_h // 3)
+    return num_shapes <= num_3x3_slots
+
+
+def part1(path):
+
+    data = tuple(load_data(path))
+    shape_masses = data[:6]
+    grid_dims_counts = data[6:]
+
+    num_can_fit = 0
+    num_cant_fit = 0
+    num_unknown = 0
+
+    for (grid_w, grid_h), counts in grid_dims_counts:
+        if enough_grid_cells(grid_w, grid_h, counts, shape_masses):
+            if can_trivially_pack(grid_w, grid_h, counts):
+                num_can_fit += 1
+            else:
+                num_unknown += 1
+        else:
+            num_cant_fit += 1
+
+    print("can fit:", num_can_fit)
+    print("can't fit:", num_cant_fit)
+    print("unknown:", num_unknown)
+
+
+if __name__ == "__main__":
+    part1("input")
